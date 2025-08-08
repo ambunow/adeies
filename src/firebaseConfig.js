9c1993905fc05dc256266101dd4a0b3a84b11230
ambunow <ambunowgr@gmail.com>
2025-08-08 18:58:00 +0300

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+// src/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbv4Wp13-0YujJUnChQOXUblwRzBX5vxk",
+  authDomain: "adeies-92384.firebaseapp.com",
+  projectId: "adeies-92384",
+  storageBucket: "adeies-92384.firebasestorage.app",
+  messagingSenderId: "78883318694",
+  appId: "1:78883318694:web:717667141b14bf5a727d67"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
